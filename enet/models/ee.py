e27aed05917491316c3ba5879c60b599ac862f70
kky <kaiyanchn@foxmail.com>
2019-10-26 19:23:11 +0800

    half store

--- enet/models/ee.py
+++ enet/models/ee.py
@@ -36,49 +36,11 @@ class EDModel(Model):
                                            device=device
                                            )
 
-        # POS-Tagging Embedding Layer
-        self.pembeddings = EmbeddingLayer(embedding_size=(hyps["pemb_size"], hyps["pemb_dim"]),
+        # Event Feature Embedding Layer
+        self.efembeddings = EmbeddingLayer(embedding_size=(hyps["ef_size"], hyps["ef_dim"]),
                                           dropout=hyps["pemb_dp"],
                                           device=device
                                           )
-
-        # Entity Label Embedding Layer
-        self.eembeddings = MultiLabelEmbeddingLayer(embedding_size=(hyps["eemb_size"], hyps["eemb_dim"]),
-                                                    dropout=hyps["eemb_dp"],
-                                                    device=device
-                                                    )
-
-        # Bi-LSTM Encoder
-        self.bilstm = DynamicLSTM(input_size=hyps["wemb_dim"] +
-                                             hyps["psemb_dim"] +
-                                             hyps["pemb_dim"] +
-                                             hyps["eemb_dim"],
-                                  hidden_size=hyps["lstm_dim"],
-                                  num_layers=hyps["lstm_layers"],
-                                  dropout=hyps["lstm_dp"],
-                                  bidirectional=True,
-                                  device=device)
-
-        # GCN
-        self.gcns = nn.ModuleList()
-        for i in range(hyps["gcn_layers"]):
-            gcn = GraphConvolution(in_features=2 * hyps["lstm_dim"],
-                                   out_features=2 * hyps["lstm_dim"],
-                                   edge_types=hyps["gcn_et"],
-                                   dropout=hyps["gcn_dp"] if i != hyps["gcn_layers"] - 1 else None,
-                                   use_bn=hyps["gcn_use_bn"],
-                                   device=device)
-            self.gcns.append(gcn)
-
-        # Highway
-        if hyps["use_highway"]:
-            self.hws = nn.ModuleList()
-            for i in range(hyps["gcn_layers"]):
-                hw = HighWay(size=2 * hyps["lstm_dim"], dropout_ratio=hyps["gcn_dp"])
-                self.hws.append(hw)
-
-        # self attention
-        self.sa = AttentionLayer(D=2 * hyps["lstm_dim"], H=hyps["sa_dim"], return_sequences=False)
 
         # Output Linear
         self.ol = BottledXavierLinear(in_features=2 * hyps["lstm_dim"], out_features=hyps["oc"]).to(device=device)
@@ -115,6 +77,10 @@ class EDModel(Model):
             ae_hidden: FloatTensor, output logits of AE, (N, output_class) or [] indicating no need to predicting arguments
             ae_logits_key: [], indicating how the big batch is constructed or []
         '''
+        
+        BATCH_SIZE = word_sequence.size()[0]
+        SEQ_LEN = word_sequence.size()[1]
+        
         # Merge embeddings
         mask = numpy.zeros(shape=word_sequence.size(), dtype=numpy.uint8)
         for i in range(word_sequence.size()[0]):
@@ -122,20 +88,14 @@ class EDModel(Model):
             mask[i, 0:s_len] = numpy.ones(shape=(s_len), dtype=numpy.uint8)
         mask = torch.ByteTensor(mask).to(self.device)
 
+        # 首先预测 trigger
         word_emb = self.wembeddings(word_sequence)
-        pos_emb = self.pembeddings(pos_tagging_sequence)
-        entity_label_emb = self.eembeddings(entity_type_sequence)
-        x_emb = torch.cat([word_emb, pos_emb, entity_label_emb], 2)  # (batch_size, seq_len, d)
-
-        BATCH_SIZE = word_sequence.size()[0]
-        SEQ_LEN = word_sequence.size()[1]
         positional_sequences = self.get_sentence_positional_feature(BATCH_SIZE, SEQ_LEN)
-        xx = []
         
         # 对每一个词进行预测
         for i in range(SEQ_LEN):
             # encoding
-            x, _ = self.bilstm(torch.cat([x_emb, self.psembeddings(positional_sequences[i].to(self.device))], 2),
+            x, _ = self.bilstm(torch.cat([word_emb, self.psembeddings(positional_sequences[i].to(self.device))], 2),
                                x_len)  # (batch_size, seq_len, d')
             # gcns
             for i in range(self.hyperparams["gcn_layers"]):
@@ -143,10 +103,17 @@ class EDModel(Model):
                     x = self.gcns[i](x, adj) + self.hws[i](x)  # (batch_size, seq_len, d')
                 else:
                     x = self.gcns[i](x, adj)
-
+    
             # self attention
             xx.append(self.sa(x, mask))  # (batch_size, d')
-            
+        
+        
+        entity_label_emb = self.eembeddings(entity_type_sequence)
+        x_emb = torch.cat([word_emb, pos_emb, entity_label_emb], 2)  # (batch_size, seq_len, d)
+
+        xx = []
+        
+        
         # output linear
         xx = torch.stack(xx, dim=1)  # (batch_size, seq_len, d')
         logits = self.ol(xx)
